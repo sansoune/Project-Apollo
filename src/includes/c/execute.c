945860b6da026dd546f5d51b95b6958bf1859520
sansoune <yassine.dakhlaoui06@gmail.com>
2022-03-26 14:45:36 +0000

    updating the insert statement

--- src/includes/c/execute.c
+++ src/includes/c/execute.c
@@ -7,8 +7,11 @@
 void execute(Statement*  statement) {
     switch (statement->type)
     {
-    case (STATEMENT_INSERT):
-        printf("it's an insert statement");
+    case (STATEMENT_INSERT): ;
+        // printf("it's an insert statement");
+        char* source = read_meta("yassine.meta");
+        printf("%s \n", source);
+        free(source);
         break;
     
     case (STATEMENT_SELECT):
